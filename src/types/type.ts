a2679b16687dc6f0c86c2e4289b33faf01ce7eeb
haiyao <haiyao70296@gmail.com>
2024-08-17 13:51:32 +1000

    feat: add type definitions

---
+++ src/types/type.ts
@@ -0,0 +1,13 @@
+type Item = {
+    name: string;
+    price: number;
+    discountFactor: number;
+    qty: number;
+    description: string;
+    rating: number;
+}
+
+type User = {
+    username: string;
+    password: string;
+}
